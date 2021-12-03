8eca4b49a04961ffbe79992528c9abcf6d5dc664
fzuycy <2253708742@qq.com>
2021-12-03 16:42:04 +0800

    Create Entry.java

---
+++ MyLMS/src/Main/Entry.java
@@ -0,0 +1,15 @@
+package Main;
+
+import view.LogIn;
+
+/**
+ * 系统入口
+ *
+ * @author 何兴旺
+ *
+ */
+public class Entry {
+    public static void main(String[] args) {
+        new LogIn().setVisible(true);
+    }
+}
